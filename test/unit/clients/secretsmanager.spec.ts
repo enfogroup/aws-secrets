ab402d4ee127ede6a9210808f4e459ac79de38a8
Urban Pettersson <urbanpe88@gmail.com>
2021-12-17 16:20:29 +0100

    test: split of Cache tests into separate suite

--- test/unit/clients/secretsmanager.spec.ts
+++ test/unit/clients/secretsmanager.spec.ts
@@ -120,46 +120,4 @@ describe('secretsmanager', () => {
       checkAllMocksCalled([getSecretMock], 1);
     });
   });
-
-  describe('getRegion', () => {
-    it('should return the current region', () => {
-      const instance = new SecretsManagerCache({ region: 'us-east-2' });
-
-      const output = instance.getRegion();
-
-      expect(output).toEqual('us-east-2');
-    });
-  });
-
-  describe('setRegion', () => {
-    it('should set the region', () => {
-      const instance = new SecretsManagerCache({ region: 'us-east-2' });
-
-      instance.setRegion('eu-west-1');
-      const output = instance.getRegion();
-
-      expect(output).toEqual('eu-west-1');
-    });
-  });
-
-  describe('getDefaultTTL', () => {
-    it('should get the default TTL', () => {
-      const instance = new SecretsManagerCache({ region: 'eu-west-1', defaultTTL: 123 });
-
-      const output = instance.getDefaultTTL();
-
-      expect(output).toEqual(123);
-    });
-  });
-
-  describe('setDefaultTTL', () => {
-    it('should set the default TTL', () => {
-      const instance = new SecretsManagerCache({ region: 'eu-west-1', defaultTTL: 123 });
-
-      instance.setDefaultTTL(456);
-      const output = instance.getDefaultTTL();
-
-      expect(output).toEqual(456);
-    });
-  });
 });
--- test/unit/clients/ssm.spec.ts
+++ test/unit/clients/ssm.spec.ts
@@ -100,46 +100,4 @@ describe('ssm', () => {
       checkAllMocksCalled([getParameterMock], 1);
     });
   });
-
-  describe('getRegion', () => {
-    it('should return the current region', () => {
-      const instance = new SSMCache({ region: 'us-east-2' });
-
-      const output = instance.getRegion();
-
-      expect(output).toEqual('us-east-2');
-    });
-  });
-
-  describe('setRegion', () => {
-    it('should set the region', () => {
-      const instance = new SSMCache({ region: 'us-east-2' });
-
-      instance.setRegion('eu-west-1');
-      const output = instance.getRegion();
-
-      expect(output).toEqual('eu-west-1');
-    });
-  });
-
-  describe('getDefaultTTL', () => {
-    it('should get the default TTL', () => {
-      const instance = new SSMCache({ region: 'eu-west-1', defaultTTL: 123 });
-
-      const output = instance.getDefaultTTL();
-
-      expect(output).toEqual(123);
-    });
-  });
-
-  describe('setDefaultTTL', () => {
-    it('should set the default TTL', () => {
-      const instance = new SSMCache({ region: 'eu-west-1', defaultTTL: 123 });
-
-      instance.setDefaultTTL(456);
-      const output = instance.getDefaultTTL();
-
-      expect(output).toEqual(456);
-    });
-  });
 });
