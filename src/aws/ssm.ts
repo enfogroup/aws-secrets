c55958ca65d24fd7651533bfda3bebc7ac7aeed6
Urban Pettersson <urbanpe88@gmail.com>
2022-06-03 07:28:54 +0200

    feat: add aws support for getting parameters by path

--- src/aws/ssm.ts
+++ src/aws/ssm.ts
@@ -36,3 +36,47 @@ export const getParameter = async (region: string, name: string, wrapper?: Wrapp
   const output = await client.getParameter({ Name: name, WithDecryption: true }).promise();
   return output.Parameter?.Value;
 };
+
+/**
+ * Retrieves SSM parameters by path
+ * @param region
+ * AWS region
+ * @param params
+ * GetParametersByPathRequest object
+ * @param wrapper
+ * Optional wrapper function to execute on all clients
+ */
+export const getPaginatedParametersByPath = async (region: string, params: SSM.GetParametersByPathRequest, wrapper?: WrapperFunction<SSM>): Promise<SSM.GetParametersByPathResult> => {
+  const client = getClient(region, wrapper);
+  const output = await client.getParametersByPath({ WithDecryption: true, ...params }).promise();
+  return output;
+};
+
+/**
+ * Retrieves SSM parameters by path, returns all values
+ * @param region
+ * AWS region
+ * @param params
+ * GetParametersByPathRequest object
+ * @param wrapper
+ * Optional wrapper function to execute on all clients
+ */
+export const getAllParametersByPath = async (region: string, params: SSM.GetParametersByPathRequest, wrapper?: WrapperFunction<SSM>): Promise<string[]> => {
+  const parameters: string[] = [];
+  let nextToken: SSM.GetParametersByPathResult['NextToken'];
+  do {
+    const response = await getPaginatedParametersByPath(region, { NextToken: nextToken, ...params }, wrapper);
+    nextToken = response.NextToken;
+
+    const newValues = (response.Parameters ?? [])
+      .reduce((aggregator: string[], value: SSM.Parameter): string[] => {
+        if (value.Value) {
+          aggregator.push(value.Value);
+        }
+        return aggregator;
+      }, []);
+
+    parameters.push(...newValues);
+  } while (nextToken);
+  return parameters;
+};
